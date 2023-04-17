030b0c69b2f72baacbec98803d69e3c30ab60a1e
Samrat <108344579+Samysr17@users.noreply.github.com>
2023-04-18 00:40:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { Counter } from './features/counter/Counter';
+import './App.css';
+// import Header from './components/Header'
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="">
+     <Home/>
+    </div>
+  );
+}
+
+export default App;
