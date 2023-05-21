18819e9596b7b05057b56ade367d81fee3a9664d
Samysr17 <108344579+Samysr17@users.noreply.github.com>
2023-05-21 17:54:57 +0530

    updated

--- src/components/Section_2_1.js
+++ src/components/Section_2_1.js
@@ -38,13 +38,13 @@ const Section_2_1 = () => {
   </div>
   <div className="text-center items-center flex  flex-col mt-[2%] md:flex-row md:text-center md:space-x-4 ">
     <Fade bottom>
-    <button className=" md:ml-[20%] sm:w-[30%] w-[80%] p-3 bg-slate-800 text-white  w- md:w-46 rounded-2xl">Order Now</button>
-    <button className=" p-3 bg-slate-300   sm:w-[30%] w-[80%] mt-4 md:mt-0 md:w-46 rounded-2xl">Demo Drive</button>
+    <button className=" md:ml-[20%] sm:w-[30%] w-[50%] p-3 bg-transparent border-2 border-white text-white rounded-md">Order Now</button>
+    <button className=" p-3 bg-white text-black  sm:w-[30%] w-[50%] mt-4 md:mt-0  rounded-md opacity-5">Demo Drive</button>
     </Fade>
   </div>
   </div>
   <div className="bg-black w-screen h-[400px] text-white flex justify-center text-center ">
-    <div className="mt-[10%] text-3xl">Interior of the Future</div>
+    <Fade bottom><div className="mt-[10%] text-3xl">Interior of the Future</div></Fade>
   </div>
   <div>
     <img className="w-screen h-screen" src={game} alt=''/>
@@ -53,9 +53,11 @@ const Section_2_1 = () => {
     <video className='w-full h-full ' src={vdo}  autoPlay muted loop/>
   </div>
   <div className="flex flex-col bg-black text-white h-[40%]">
-  <h1 className ="text-xl ml-[10%] mt-16 ">Cinematic Experience</h1>
+  <Fade bottom> <h1 className ="text-xl ml-[10%] mt-16 ">Cinematic Experience</h1>
 <p className="text-sm ml-[10%] mt-8 max-w-[80%] mb-[100px]">A 17” touchscreen with left-right tilt offers 2200 x 1300 resolution, true colors and exceptional responsiveness for gaming, movies and more.</p>
+</Fade>
   </div>
+
   <div>
     <img className="w-screen h-screen" src={con} alt=''/>
   </div>
@@ -63,13 +65,17 @@ const Section_2_1 = () => {
     
   </div>
   <div className="bg-black text-white flex-col">
+  <Fade bottom>
   <p className="pt-4 text-xl ml-[10%]">Plaid</p>
   <h1 className="mt-4 font-semibold text-2xl ml-[10%]">Beyond Ludicrous</h1>
   <p className='max-w-[60%] pt-4 ml-[10%] text-sm'>Model S Plaid has the quickest acceleration of any vehicle in production. Updated battery architecture for all Model S trims enables back-to-back track runs without performance degradation. Chat with a Tesla Advisor to learn more about Model S or schedule a demo drive today.</p>
+  </Fade>
+  <Fade bottom>
   <div className="flex  max-w-[70%] md:max-w-[20%] justify-between ml-[10%] mt-12 ">
   <button className='p-2 bg-transparent border-2 border-white rounded-md'>Order Now</button>
   <button className='p-2 bg-white text-black border-2  border-white rounded-md '>Compare Models</button>
   </div>
+  </Fade>
   </div>
   <div>
     <img className="w-screen h-screen" src={foot} alt=''/>
