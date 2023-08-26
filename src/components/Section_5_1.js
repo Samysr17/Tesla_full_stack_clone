b5ef815aa1eef383d25ce99b724d407298c52919
Samysr17 <108344579+Samysr17@users.noreply.github.com>
2023-08-26 10:19:24 +0530

    modifications

--- src/components/Section_5_1.js
+++ src/components/Section_5_1.js
@@ -13,23 +13,23 @@ const Section_5_1 = () => {
     <div className="text-center items-center flex flex-col  mt-[35%] md:mt-[20%] md:flex-row md:text-center ">
     <Fade bottom>
     <button className=" md:ml-[10%] md:mr-[2%] bg-white border-black border-4 hover:ease-in duration-300 hover:w-[70%]    w-[50%] p-3  hover:text-white hover:bg-black  rounded-2xl">Order Now</button>
-    <button className=" p-3 bg-transparent border-white text-white hover:bg-white hover:ease-in duration-300 hover:w-[70%]  hover:text-black border-4 md:mr-[10%] opacity-25  md:ml-[2%]  w-[50%] mt-4 md:mt-0  rounded-2xl">Demo Drive</button>
+    <button className=" p-3 bg-transparent border-white  text-white hover:bg-white hover:ease-in duration-300 hover:w-[70%]  hover:text-black border-4 md:mr-[10%] opacity-25  md:ml-[2%]  w-[50%] mt-4 md:mt-0  rounded-2xl">Demo Drive</button>
     </Fade> 
     </div>
     </div>
      <div className='bg-black h-screen w-screen bg-cover bg-center'>
-     <div className='p-[20vh] text-center flex flex-col justify-center items-center '>
+     <div className=' p-[20vh] text-center items-center flex flex-col'>
         <h1 className='text-white text-3xl'>Transform Your Roof</h1>
-        <button className="bg-transparent mt-8 border-white border-4 hover:ease-in duration-300 hover:w-[35%] w-[30%]  p-2 text-white hover:bg-white hover:text-black  rounded-sm">Order Now</button>
-        <button className="bg-gray-600 opacity-75 hover:opacity-100 mt-8 text-white hover:ease-in duration-300 hover:w-[35%] border-4 border-gray-600 w-[30%] p-2   rounded-sm">Schedule a Virtual Consultation</button>
+        <button className="bg-transparent mt-8 border-white border-4 hover:ease-in duration-300 hover:w-[35%] w-100vh  md:w-[50%] p-2  text-white hover:bg-white hover:text-black  rounded-sm">Order Now</button>
+        <button className="bg-gray-600 opacity-75 hover:opacity-100 mt-8 text-white hover:ease-in duration-300 hover:w-[35%] border-4 border-gray-600  md:w-[50%] p-2   rounded-sm">Schedule a Virtual Consultation</button>
         <span className="text-sm underline text-white mt-4 cursor-pointer hover:text-xl hover:ease-in duration-300 ">Get Updates</span>
       </div>
-      <div className="flex mt-[10%] justify-center text-white">
+      <div className="sm:flex mt-[10%] justify-center hidden text-white space-x-4 ">
         <div>Tesla © 2023</div>
         <div>Privacy & Legal</div>
         <div>Vehicle Recalls</div>
         <div>Contact</div>
-        <div>Location</div>
+        <div className="">Location</div>
       </div>
     </div>
     </div>
