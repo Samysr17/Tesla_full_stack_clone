af858c33342391b45f3351ffe74eab7d0a00e08c
Samysr17 <108344579+Samysr17@users.noreply.github.com>
2023-08-30 21:19:59 +0530

    updated routes

---
+++ src/components/addtocart_home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header'
+
+const addtocart_home = () => {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+export default addtocart_home
