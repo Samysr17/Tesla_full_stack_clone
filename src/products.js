0fd99fa14914b4cfce7292629e6315f27a357d75
Samysr17 <108344579+Samysr17@users.noreply.github.com>
2023-08-31 11:04:51 +0530

    Data added

---
+++ src/products.js
@@ -0,0 +1,43 @@
+export const products=[
+     {
+        id:1,
+        Name:"Model X",
+        Price:150000.0,
+        Colour:["Red","Blue","Green","Black"],
+        Image:""
+
+     },
+     {
+        id:1,
+        Name:"Model Y",
+        Price:500000.0,
+        Colour:["Red","Blue","Green","Black"],
+        Image:""
+
+     },
+     {
+        id:1,
+        Name:"Solar Panel",
+        Price:3000.0,
+        Colour:["Black","Grey"],
+        Image:""
+
+     },
+     {
+        id:1,
+        Name:"Solar Roof",
+        Price:20000.0,
+        Colour:["Black","Grey"],
+        Image:""
+
+     },
+     {
+        id:1,
+        Name:"PowerWall",
+        Price:10000.0,
+        Colour:["Black","Grey"],
+        Image:""
+
+     }
+
+]
