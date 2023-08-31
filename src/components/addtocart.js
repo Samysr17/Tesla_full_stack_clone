31b29f857d94b7c965ea3aea0e75b1710d12a01c
Samysr17 <108344579+Samysr17@users.noreply.github.com>
2023-08-31 20:44:57 +0530

    functionalities added

--- src/components/addtocart.js
+++ src/components/addtocart.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import {useContext} from 'react'
+import { products } from '../products'
+import { Addtocart_context } from '../context/addtocart-context'
+const Addtocart = () => {
+  const {items}=useContext(Addtocart_context);
 
-const addtocart = () => {
   return (
+    <div>
     <div className="text-xl">addtocart</div>
+    <div>
+    {products.map((iterator)=>{
+      if(items[1]!==0){
+        return(
+          <div className='text-xl'>item</div>
+        )
+
+      }
+    })}
+    </div>
+    </div>
   )
 }
 
-export default addtocart
+export default Addtocart
