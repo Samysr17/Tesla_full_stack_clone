ce1f30d2b0b315df231f42aafe8e797975819d3d
Samysr17 <108344579+Samysr17@users.noreply.github.com>
2023-08-31 21:44:03 +0530

    functionality added

--- src/components/Cartitem.js
+++ src/components/Cartitem.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
-const Cartitem = () => {
+const Cartitem = (props) => {
+  const {id,Name,Price,Image}=props.data;
   return (
-    <div>Cartitem</div>
+    <div>
+      <div className="flex justify-center mt-[20%]">
+    <div>{Name}</div>
+    <img className="w-[5%] h-[5%]" src={Image}></img>
+    <div>{Price}</div>
+    </div>
+    </div>
   )
 }
 
--- src/components/addtocart.js
+++ src/components/addtocart.js
@@ -2,24 +2,20 @@ import React from 'react'
 import {useContext} from 'react'
 import { products } from '../products'
 import { Addtocart_context } from '../context/addtocart-context'
-import {Cartitem} from './Cartitem'
+import Cartitem from './Cartitem'
 const Addtocart = () => {
   const {items}=useContext(Addtocart_context);
 
   return (
     <div>
-    <div className="text-xl">addtocart</div>
-    <div>
     {products.map((iterator)=>{
       if(items[iterator.id]!==0){
         return(
-          // <Cartitem data={iterator}/>
-          <div>1</div>
+         <Cartitem data={iterator}/>
         )
 
       }
     })}
-    </div>
     </div>
   )
 }
