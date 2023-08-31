8e23f5204008a425adefbf7423921da41d7bb3f8
Samysr17 <108344579+Samysr17@users.noreply.github.com>
2023-08-31 21:06:07 +0530

    route func done console

---
+++ src/components/Cartitem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cartitem = () => {
+  return (
+    <div>Cartitem</div>
+  )
+}
+
+export default Cartitem
--- src/components/addtocart.js
+++ src/components/addtocart.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {useContext} from 'react'
 import { products } from '../products'
 import { Addtocart_context } from '../context/addtocart-context'
+import {Cartitem} from './Cartitem'
 const Addtocart = () => {
   const {items}=useContext(Addtocart_context);
 
@@ -10,9 +11,10 @@ const Addtocart = () => {
     <div className="text-xl">addtocart</div>
     <div>
     {products.map((iterator)=>{
-      if(items[1]!==0){
+      if(items[iterator.id]!==0){
         return(
-          <div className='text-xl'>item</div>
+          // <Cartitem data={iterator}/>
+          <div>1</div>
         )
 
       }
