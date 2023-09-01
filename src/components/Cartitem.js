e71039a3815da2e8646c02f46155deed99ed3e4f
Samysr17 <108344579+Samysr17@users.noreply.github.com>
2023-09-01 22:38:07 +0530

    updated routes

--- src/components/Cartitem.js
+++ src/components/Cartitem.js
@@ -5,7 +5,7 @@ const Cartitem = (props) => {
   return (
     <div>
       <Fade left>
-      <div className="flex flex-col md:flex-row justify-between md:p-[10vh]  ">
+      <div className="flex flex-col md:flex-row justify-between md:p-[10vh] border-2 border-white  ">
       <div>
       <img className="md:w-[30%] md:h-[100%] w-screen h-[100%] " src={Image} alt="/"></img>
       </div>
@@ -13,10 +13,12 @@ const Cartitem = (props) => {
     <div className="text-white">{Name}</div>
     <div className="text-white  " >${Price}</div>
     </div>
+    <div className='md:flex justify-center space-x-4 hidden '>
+    <div className="text-white text-xl">{Name}</div>
+    <div className="text-white text-xl" >${Price}</div>
+    <button className="p-1 h-8 w-10 text-black bg-white text-xl ">+</button>
+    <button className="p-1 h-8 w-10 text-black bg-white text-xl">+</button>
     </div>
-    <div className='md:flex justify-between hidden '>
-    <div className="text-white">{Name}</div>
-    <div className="text-white  " >${Price}</div>
     </div>
     </Fade>
     </div>
