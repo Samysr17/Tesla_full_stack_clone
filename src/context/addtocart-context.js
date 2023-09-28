765ef87f3e27f7329efb769a8d516d652014bb00
Samysr17 <108344579+Samysr17@users.noreply.github.com>
2023-09-28 23:25:45 +0530

    Update addtocart-context.js

--- src/context/addtocart-context.js
+++ src/context/addtocart-context.js
@@ -1,6 +1,6 @@
 import React, { createContext,useState,useEffect } from 'react'
 import { products } from '../products'
-export const Addtocart_context=createContext(null)
+export const Addtocart_context=createContext(null)//to do add to cart full functionality // firebase auth //complete final
 const initial_state=()=>{
   let cart={}
     for(let i=1;i<=products.length;i++){
