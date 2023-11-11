cb2626bee11152a714c632ec0e24032b323b84ea
Samysr17 <108344579+Samysr17@users.noreply.github.com>
2023-11-11 11:13:45 +0530

    payment gateway

--- src/components/addtocart.js
+++ src/components/addtocart.js
@@ -5,6 +5,7 @@ import { Addtocart_context } from '../context/addtocart-context'
 import Cartitem from './Cartitem'
 import { Link } from 'react-router-dom'
 import { Fade } from 'react-reveal'
+import {loadStripe} from '@stripe/stripe-js';
 const Addtocart = () => {
   const {items}=useContext(Addtocart_context);
   const {getotal}=useContext(Addtocart_context)
@@ -20,6 +21,27 @@ const Addtocart = () => {
       );
     }
   }
+  // const payment=async()=>{
+  //   const stripe = await loadStripe('pk_test_51OB9TcSB3m3uX235oYnbAGt7I1TflMXxSLco872UxB27EUY0KqPVTnXHR9z8V5OxPbeV0ZQpYz7rWDY7UKsTPriH005xaPamUu');
+  //   const body={
+  //     products:carts
+  //   }
+  //   const headers={
+  //     "Content-Type":"application/json"
+  //   }
+  //   const response=await fetch("http://localhost:7000/api/create-checkout-session",{
+  //     method:"POST",
+  //     headers:headers,
+  //     body:JSON.stringify(body)
+  //   });
+  //   const session=await response.json();
+  //   const result=stripe.redirectToCheckout({
+  //     sessionId:session.id
+  //   })
+  //   if(result.error){
+  //     console.log(result.error)
+  //   }
+  // }
   return (
     <div>
     <div className="flex justify-between bg-black">
@@ -40,7 +62,7 @@ const Addtocart = () => {
       }
     })}
     </div>
-    <div className='flex bg-black max-h-[500vh] w-[40%] sticky '>
+    {/* <div className='md:hidden flex bg-black max-h-[500vh] w-[40%] sticky '>
       <div className="w-[90%] hidden  md:flex flex-col space-y-8 justify-center mt-[50%]  ">
       <p className='text-white  text-2xl text-center  '>Order Summary</p>
       <div className="flex justify-between border-t-2">
@@ -59,7 +81,7 @@ const Addtocart = () => {
       <button  className=' bg-transaprent text-white border-2 border-white p-3 rounded-md text-xl hover:w-[80%] hover:ml-[10%] hover:ease-in cursor-pointer'>Checkout</button>
       <button className='  bg-white p-3 rounded-md text-xl hover:w-[80%] hover:ml-[10%] hover:ease-in cursor-pointer'><Link to="/">Continue Shopping</Link></button>
       </div>
-    </div>
+    </div> */}
     <Fade right>
     <div className='hidden md:flex bg-black max-h-[500vh] w-[40%] sticky '>
       <div className="w-[90%] hidden  md:flex flex-col space-y-8 justify-center mt-[50%]  ">
